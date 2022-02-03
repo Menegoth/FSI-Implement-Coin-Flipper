2ebd24400ed01d8c32f3bbef72ac392295f92352
Menegoth <antonioareas@me.com>
2022-02-02 21:34:46 -0800

    Completed assignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,67 @@
 // TODO: Declare any global variables we need
-
+let totalHeads = 0;
+let totalTails = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+
+    //DECLARE DOM ELEMENTS
+    //image and message
+    const pennyImage = document.getElementById("penny-image");
+    const message = document.getElementById("message");
+
+    //table
+
+    //FLIP BUTTON
+    document.getElementById("flip").addEventListener("click", () => {
+
+        //determine roll, less than 0.5 is heads; update message and table
+        if (Math.random() < 0.5) {
+            pennyImage.src = "assets/images/penny-heads.jpg";
+            message.textContent = "You Flipped Heads";
+            totalHeads++;
+        } else {
+            pennyImage.src = "assets/images/penny-tails.jpg";
+            message.textContent = "You Flipped Tails";
+            totalTails++;
+        }
+
+        //update scoreboard
+        //save total
+        let total = totalHeads + totalTails;
+
+        //store percentages
+        let headsPercent = 0;
+        let tailsPercent = 0;
+
+        if (total > 0) {
+            headsPercent = Math.round((totalHeads / total) * 100);
+            tailsPercent = Math.round((totalTails / total) * 100);
+        }
+
+        //update scoreboard values
+        document.getElementById("heads").textContent = totalHeads;
+        document.getElementById("heads-percent").textContent = `${headsPercent}%`
+        document.getElementById("tails").textContent = totalTails;
+        document.getElementById("tails-percent").textContent = `${tailsPercent}%`
+
+    })
+
+    //CLEAR BUTTON
+    document.getElementById("clear").addEventListener("click", () => {
+
+        //reset global variables
+        totalHeads = 0;
+        totalTails = 0;
+
+        //update message
+        message.textContent = "Let\'s Get Rolling!";
+
+        //update scoreboard
+        document.getElementById("heads").textContent = "0";
+        document.getElementById("heads-percent").textContent = "0%";
+        document.getElementById("tails").textContent = "0";
+        document.getElementById("tails-percent").textContent = "0%";
+    })
 
     // TODO: Add event listener and handler for flip and clear buttons
 
